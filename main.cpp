afb2283b6d40fb67a994a40b9e12eec4f108f1a2
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-12-02 09:54:09 +0100

    Added new

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main(){
+
+    float length;
+    float width;
+    float height;
+
+    std::cout   << "Welcome to the box calculator. Please type in length, width and height information." << std::endl;
+    std:: cout  <<   "Length : ",  std::cin >> length;
+    std:: cout  <<   "Width : ",   std::cin >> width;
+    std:: cout  <<   "Height : ",  std::cin >> height;
+
+    const float base = length * width;
+
+    std::cout   << "The base area is : "  << base << std::endl;
+    std::cout   << "The volume is : "     << (base*height)  << std::endl;
+
+    return 0;
+}
